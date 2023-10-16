d38bdc700e7e8c4dde7c0d70afcf08912e214986
jjguillen <jjavierguillen@gmail.com>
2023-10-16 18:02:48 +0200

    php with mysql

--- tema4/prueba.php
+++ tema4/prueba.php
@@ -2,9 +2,15 @@
 
     $dbh = null;
 
+    define('DB_NAME', 'php');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'toor');
+    define('DB_HOST', '172.17.0.3:3306'); //La IP del contenedor Mysql, y el puerto interno del contenedor
+
     try {
-        $dsn = "mysql:host=127.0.0.1:3306;dbname=php";
-        $dbh = new PDO($dsn, "root", "toor");
+        $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     } catch (PDOException $e){
         echo $e->getMessage();
     }
@@ -15,5 +21,11 @@
     $stmt->setFetchMode(PDO::FETCH_ASSOC);
     // Ejecutamos
     $stmt->execute();
+
+    while($row = $stmt->fetch()){
+        echo "Nombre: " . $row['nombre'] . "<br>";
+        echo "Email: " . $row['email'] . "<br>";
+    }
+    
 
 ?>
