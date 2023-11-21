12250a2f8f391692474acafb917423c8545defb1
jjguillen <jjavierguillen@gmail.com>
2023-11-21 17:20:10 +0100

    chatgpt

--- tema6/apichatgpt/index.php
+++ tema6/apichatgpt/index.php
@@ -8,10 +8,10 @@ $client = new \GuzzleHttp\Client();
 $texto = "Los coleccionistas de juegos retro";
 $textoArticulo = "Escribe un artículo sobre " . $texto;
 
-$response = $client->request('POST', 'https://api.openai.com/v1/completions', [
-  'body' => '{"model": "text-davinci-003", "prompt": "'.$textoArticulo.'", "temperature": 0, "max_tokens": 1000, "n": 1}',
+$response = $client->request('POST', 'https://api.openai.com/v1/chat/completions', [
+  'body' => '{"model": "gpt-3.5-turbo", "temperature": 0,  "messages": [{"role": "user", "content": "'.$textoArticulo.'"}]}',
   'headers' => [
-    'Authorization' => 'Bearer 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',
+    'Authorization' => 'Bearer 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',
     'accept' => 'application/json',
     'content-type' => 'application/json',
   ],
@@ -24,11 +24,12 @@ $respuestaJSON = json_decode($respuesta);
 echo $respuestaJSON->choices[0]->text;
 echo "<br>";
 
+/*
 $textoImagen = $texto;
 $response = $client->request('POST', 'https://api.openai.com/v1/images/generations', [
   'body' => '{"prompt": "'.$textoImagen.'", "size": "1024x1024", "n": 1}',
   'headers' => [
-    'Authorization' => 'Bearer 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',
+    'Authorization' => 'Bearer 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',
     'accept' => 'application/json',
     'content-type' => 'application/json',
   ],
@@ -41,6 +42,6 @@ $respuestaJSON = json_decode($respuesta);
 echo $respuestaJSON->data[0]->url;
 
 echo "<a href='pruebaConexion.php?imagen=".urlencode($respuestaJSON->data[0]->url)."'>Imagen</a>";
-
+*/
 ?>
    
