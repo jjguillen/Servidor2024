0ad300970b4b309b5ace2cd057f3bdfe63992b87
jjguillen <jjavierguillen@gmail.com>
2023-12-06 00:43:02 +0100

    apinode conectar a mongo

--- tema6/apinodejs/index.js
+++ tema6/apinodejs/index.js
@@ -1,12 +1,9 @@
 const app = require("./app");
 const port = 3000;
 const mongoose = require("mongoose");
-const urlMongo = "mongodb://root:toor@localhost:27018/apitareas";
+const urlMongo = "mongodb://root:toor@localhost:27017/apitask?authSource=admin";
 
-mongoose.connect(urlMongo,
-  {
-    useUnifiedTopology: true
-  });
+mongoose.connect(urlMongo);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
