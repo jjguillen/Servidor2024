5fdab9cf19d4dc902b5ebf206864dca227752568
jjguillen <jjavierguillen@gmail.com>
2023-12-13 22:40:11 +0100

    cambios api cors

--- tema6/apinodejs/app.js
+++ tema6/apinodejs/app.js
@@ -5,6 +5,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+//Soluciona el problea de CORS.
+//Permitir peticiones a la API desde ese dominio. Poner *, para atender peticiones desde cualquier punto.
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.header(
+      "Content-Type","Content-Length","Server","Date","Access-Control-Allow-Methods","Access-Control-Allow-Origin"
+    );
+    next();
+  });
+  
 //Cargar rutas
 const task_rutas = require("./routes/task");
 
