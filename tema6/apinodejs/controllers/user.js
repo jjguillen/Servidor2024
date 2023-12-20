004bc49344109cf36eb7fbc7fd8360d6280d694a
jjguillen <jjavierguillen@gmail.com>
2023-12-20 17:04:17 +0100

    apinodejs

--- tema6/apinodejs/controllers/user.js
+++ tema6/apinodejs/controllers/user.js
@@ -1,11 +1,59 @@
 const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
+const jwt = require("../services/jwt");
 
 //Función para registrar un usuario
-function register(req, res) { 
-    console.log("Registrando usuario");
+async function register(req, res) { 
+    //Creamos el objeto usuario con los datos del body (Json con el user)
+    const user = new User(req.body);
+
+    try {
+        if (!user.email) throw { msg: "El email es obligatorio" };
+        if (!user.password) throw { msg: "El password es obligatorio" };
+
+        //Ver si el email está ya registrado
+        const foundEmail = await User.findOne({ email: user.email });
+        if (foundEmail) throw { msg: "El email ya está en uso" };
+
+        //Encriptar contraseña
+        const salt = bcryptjs.genSaltSync(10);
+        user.password = await bcryptjs.hash(user.password, salt);
+
+        //Insertamos el usuario
+        const userStore = await user.save();
+        if (!userStore) {
+            res.status(400).send({msg: "Error en MongoDB"});
+        } else {
+            res.status(201).send({user : userStore});
+        }
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
 }
 
+async function login(req, res) {
+    const { email, password } = req.body;
+
+    try {
+        //Busco el usuario con ese email en la BBDD
+        const user = await User.findOne( { email: email });
+
+        //Email no encontrado, error
+        if (!user) throw { msg: "Error en login " };
+
+        //Comprobamos que la contraseña sea igual que la de la BBDD
+        const passwordSuccess = await bcryptjs.compare(password, user.password);
+        if (!passwordSuccess) throw { msg: "Error en login" };
+
+        res.status(200).send({ token: jwt.createToken(user, "12h") });
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
 
 module.exports = {
-    register, 
+    register, login
 }
--- tema6/apinodejs/routes/user.js
+++ tema6/apinodejs/routes/user.js
@@ -4,5 +4,6 @@ const api = express.Router();
 
 //Rutas
 api.post("/register", UserController.register); //Registrar usuario
+api.post("/login", UserController.login); //Login de usuario
 
 module.exports = api;
